ae3ed9bfb410f666c97dcc2bb13fd0f346acefa3
franciscochang@protonmail.com <franciscochang@protonmail.com>
2022-10-14 19:10:36 -0300

    actualizar ejercicio 6

--- src/ej06/Dado.java
+++ src/ej06/Dado.java
@@ -2,15 +2,31 @@ package ej06;
 
 public class Dado {
 	
-	private int valor;
+	private static final int CANTIDAD_DE_CARAS = 6;
+	             private int valor;
+	             private int cantidadDeCaras;
 	
 	public Dado() {
+		this.setCantidadDeCaras(cantidadDeCaras);
+		this.tirar();
+	}
+
+	private int tirar() {
+		this.valor = this.calcularValor();
+		return this.valor;
 		
-		valor = (int)(Math.random() * 6);
+	}
+
+	private int calcularValor() {
+		return ((int)(Math.random() * this.cantidadDeCaras)); 
 	}
 
 	public int getValor() {
-		return valor;
+		return this.valor;
+	}
+
+	public void setCantidadDeCaras(int cantidadDeCaras) {
+		this.cantidadDeCaras = CANTIDAD_DE_CARAS;
 	}
 
 	
